13412a3ecc8ead0f56e4f77b487f8cdcebb348ff
iiPython <ben@iipython.dev>
2024-05-19 15:09:44 -0500

    svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<script>
+    import { page } from "$app/stores";
+
+    $: path = $page.url.pathname.split("/")[1];
+    $: title = path ? `iiPython / ${path[0].toUpperCase()}${path.slice(1)}` : "iiPython";
+</script>
+
+<slot />
