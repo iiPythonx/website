8352c530c3eaf81d9dd36042fc739c96a3f14f08
iiPython <ben@iipython.dev>
2024-08-28 12:46:02 -0500

    fixup

--- src/static/js/snowball.js
+++ src/static/js/snowball.js
@@ -1,12 +1,13 @@
-// Handle snowball
-const bear = document.getElementById("snowball");
-var rotation = 0;
-bear.addEventListener("click", () => {
-    bear.classList.add("move");
-    rotation += 3600;
-    bear.style.rotate = `${rotation}deg`;
-});
-bear.addEventListener("transitionend", () => {
-    bear.classList.remove("move");
-    (rotation = 0), (bear.style.rotate = "0deg");
-});
+(() => {
+    const bear = document.getElementById("snowball");
+    var rotation = 0;
+    bear.addEventListener("click", () => {
+        bear.classList.add("move");
+        rotation += 3600;
+        bear.style.rotate = `${rotation}deg`;
+    });
+    bear.addEventListener("transitionend", () => {
+        bear.classList.remove("move");
+        (rotation = 0), (bear.style.rotate = "0deg");
+    });
+})();
