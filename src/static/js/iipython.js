490baff505b87fee1a33cdffd0cce5ae77400b67
iiPython <ben@iipython.dev>
2024-09-30 19:07:09 -0500

    fallback links to use last.fm

--- src/static/js/iipython.js
+++ src/static/js/iipython.js
@@ -28,14 +28,19 @@
             if (!data) { now_playing.innerHTML = "Nothing playing."; return; };
             if (data.mbid.track && data.mbid.track === last_track.mbid.track) return;
         
+            // Figure out URLs
+            const album_url = data.mbid.album ? `https://musicbrainz.org/release/${data.mbid.album}` : `https://last.fm/music/${data.artist}/${data.album}`;
+            const artist_url = data.mbid.artist ? `https://musicbrainz.org/artist/${data.mbid.artist}` : `https://last.fm/music/${data.artist}`;
+            const track_url = data.mbid.track ? `https://musicbrainz.org/recording/${data.mbid.track}` : `https://last.fm/music/${data.artist}/_/${data.track}`;
+
             // Push to page
             last_track = data;
-            const track_link = `<a href = "https://musicbrainz.org/recording/${data.mbid.track}">${data.track}</a>`;
+            const track_link = `<a href = "${track_url}">${data.track}</a>`;
             now_playing.innerHTML = `
-                <a href = "https://musicbrainz.org/release/${data.mbid.album}"><img src = "${data.image}"></a>
+                <a href = "${album_url}"><img src = "${data.image}"></a>
                 <div>
                     ${data.track.length > 15 ? `<marquee scrollamount = "3">${track_link}</marquee>` : `<span>${track_link}</span>`}
-                    <span><a href = "https://musicbrainz.org/artist/${data.mbid.artist}">${data.artist}</a></span>
+                    <span><a href = "${artist_url}">${data.artist}</a></span>
                 </div>
             `;
         });
