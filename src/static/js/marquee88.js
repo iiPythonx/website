cc6546f8f91bc61f1971024e4aa220b4aca83a18
iiPython <ben@iipython.dev>
2025-02-20 21:42:34 -0600

    smooth out the marquee

--- src/static/js/marquee88.js
+++ src/static/js/marquee88.js
@@ -5,28 +5,34 @@
 
     // Configuration
     const gap = 10;  // Gap between buttons, in pixels
+    const amount = 1;  // Pixels to move per animation frame
     const element = document.querySelector(".m88");  // Main element you want to select
 
     // Ensure position is relative so we can use `left`
     const box = element.querySelector("div");
     box.style.position = "relative";
 
-    // Setup interval
-    let left = 0;
-    interval = setInterval(() => {
+    // Setup animation
+    let left = 0, last = 0;
+    function frame(time) {
+        if (time - last >= 10) {
 
-        // Calculate bounding boxes
-        const truth = element.getBoundingClientRect().left;
-        const rect = box.children[0].getBoundingClientRect();
-
-        // Push left button to right side
-        if ((rect.left + rect.width) <= truth) {
-            box.appendChild(box.children[0]);
-            left += rect.width + gap;
+            // Calculate bounding boxes
+            const truth = element.getBoundingClientRect().left;
+            const rect = box.children[0].getBoundingClientRect();
+        
+            // Push left button to right side
+            if ((rect.left + rect.width) <= truth) {
+                box.appendChild(box.children[0]);
+                left += rect.width + gap;
+            }
+        
+            // Update box positioning
+            box.style.left = `${left}px`;
+            left -= amount;
+            last = time;
         }
-
-        // Update box positioning
-        box.style.left = `${left}px`;
-        left--;
-    }, 20);
+        requestAnimationFrame(frame);
+    }
+    requestAnimationFrame(frame);
 })();
