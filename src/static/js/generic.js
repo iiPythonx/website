7b6ffb643dba5ccba04791d581242c72030ae9eb
iiPython <ben@iipython.dev>
2025-11-13 19:51:41 -0600

    remove read tracker and add beacon

--- src/static/js/generic.js
+++ src/static/js/generic.js
@@ -14,27 +14,6 @@
         e.currentTarget.innerText = names[index];
     });
 
-    // Handle reading
-    const content = document.getElementById("content");
-    const calculate_progress = () => content.scrollTop + content.clientHeight;
-    const redraw = () => {
-        if (calculate_progress() >= content.scrollHeight) return;
-        const reading_progress = document.createElement("span");
-        reading_progress.classList.add("read-progress")
-        reading_progress.innerText = `${Math.round((calculate_progress() / content.scrollHeight) * 100)}%`;
-        content.appendChild(reading_progress);
-    };
-    content.addEventListener("scroll", () => {
-        const progress = content.querySelector(".read-progress");
-        if (!progress) return;
-        progress.innerText = `${Math.round((calculate_progress() / content.scrollHeight) * 100)}%`;
-    });
-    (new MutationObserver((mutations) => {
-        if (mutations[0].addedNodes.length && mutations[0].addedNodes[0].className === "read-progress") return;
-        redraw();
-    })).observe(content, { childList: true });
-    setTimeout(redraw, 50);
-
     // Handle SPA
     const pages = ["/", "/about", "/music", "/projects", "/archives", "/anime"];
     const length = location.origin.length;
@@ -80,6 +59,9 @@
         });
 
         if (window.clock_tick) window.clock_tick();
+
+        // Handle beacon bc yuh
+        navigator.sendBeacon("https://ghost.iipython.dev/cheesepizza", window.location.href);
     }
     setup_links(document);
 
