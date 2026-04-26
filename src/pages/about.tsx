3a732588cc5e6785e1f8c6ae41528cc86b9578fc
iiPython <ben@iipython.dev>
2026-04-26 05:09:55 -0500

    some css simplification

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,7 @@ import { Link } from "wouter";
 
 export function AboutPage() {
     return <>
-        <h2>Kachow!</h2>
+        <h2 class = "page-title">Kachow!</h2>
         <p>
             I'm Benjamin: a software developer, system administrator, and cybersecurity student. <br />
             I like programming, configuring my homelab, and listening to music.
