b24fe1a29da0ff6c286616f7794b5ad8bd91dbf2
iiPython <ben@iipython.dev>
2026-04-26 05:49:36 -0500

    good start to main

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,28 @@
 import { Link } from "wouter";
 
+import GitHubIcon from "../assets/icons/github.svg?react";
+import DiscordIcon from "../assets/icons/discord.svg?react";
+import AniListIcon from "../assets/icons/anilist.svg?react";
+import SteamIcon from "../assets/icons/steam.svg?react";
+
+const ACCOUNT_LIST = [
+    { name: "GitHub",  link: "https://github.com/iiPythonx",                 user: "iiPythonx", icon: GitHubIcon },
+    { name: "Discord", link: "https://discord.com/users/633185043774177280", user: "iiPython",  icon: DiscordIcon },
+    { name: "AniList", link: "https://anilist.co/user/iiPython/",            user: "iiPython",  icon: AniListIcon },
+    { name: "Steam",   link: "https://steamcommunity.com/id/iiPython_",      user: "iiPython_", icon: SteamIcon },
+]
+
+function AccountList() {
+    return ACCOUNT_LIST.map((account) => {
+        const Icon = account.icon;
+        return <div style = {{ display: "flex", gap: "6px", alignItems: "center" }}>
+            <Icon />
+            <span style = {{ width: "70px" }}>{account.name}:</span>
+            <span><a href = {account.link}>@{account.user}</a></span>
+        </div>
+    });
+}
+
 export function AboutPage() {
     return <>
         <h2 class = "page-title">Kachow!</h2>
@@ -8,6 +31,13 @@ export function AboutPage() {
             I like programming, configuring my homelab, and listening to music.
         </p>
         <br />
+        <p>
+            The iiPython username was inspired by <a href = "https://en.wikipedia.org/wiki/Pythonidae">Pythons</a>, not the programming language.
+            Although I do have around 12 years of experience with <a href = "https://python.org">Python</a> so you might as well assume they're correlated somehow.
+        </p>
+        <br />
         <p>For ways to contact me, check out my <Link href = "/contact">contact page</Link>!</p>
+        <h2 class = "page-title">Accounts</h2>
+        <AccountList />
     </>
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,14 @@
 export function ContactPage() {
-    return <span>Contact page.</span>;
+    return <>
+        <h2 class = "page-title">Contact Me</h2>
+        <p>
+            The best and easiest way will always be to send me a message on <a href = "https://discord.com/users/633185043774177280">Discord</a> (@iiPythonx). <br />
+            Moving on though, you can always shoot me an email at <a href = "mailto:ben@iipython.dev">ben@iipython.dev</a>.
+        </p>
+        <br />
+        <p>
+            You can send me encrypted email using <a href = "/keys/pgp.txt">my GPG key</a>, attach your keys if you want an encrypted response. <br />
+            And to ensure I'm not missing anything: here's <a href = "/keys/ed25519.txt">my SSH key</a> too, for commit verification or (insert something cool here).
+        </p>
+    </>
 }
